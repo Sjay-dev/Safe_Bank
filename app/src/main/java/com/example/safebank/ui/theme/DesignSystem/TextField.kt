b171e7db2734a5596b44649be7b7bafb8cb04d4f
Joseph Sanusi <josephsanusi4@gmail.com>
2026-01-20 17:33:12 +0100

    TextFiled Updated

--- app/src/main/java/com/example/safebank/ui/theme/DesignSystem/TextField.kt
+++ app/src/main/java/com/example/safebank/ui/theme/DesignSystem/TextField.kt
@@ -4,6 +4,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -16,6 +20,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
@@ -72,10 +77,14 @@ fun TextField(
                     ) {
                         when{
                         isPasswordVisible ->{
-
+                          Icon(
+                              imageVector = Icons.Default.Visibility , contentDescription = "Hide Password"
+                          )
                             }
                              !isPasswordVisible ->{
-
+                                 Icon(
+                                     imageVector = Icons.Default.VisibilityOff , contentDescription = "Show Password"
+                                 )
                             }
                         }
                     }
